7b0fdc79575b2fcfac7c1339b52fe01f700ae750
marco <a0931900802@gmail.com>
2025-04-18 14:31:44 +0800

    feat: add RainbowProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+// web3 provider
+import { RainbowProvider } from "@/context/ranbow";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +30,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <RainbowProvider>
+          {children}
+        </RainbowProvider>
       </body>
     </html>
   );
