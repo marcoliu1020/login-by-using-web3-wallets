610ec9269664e8aff367b275260096a8b9004191
marco <a0931900802@gmail.com>
2025-04-20 20:26:35 +0800

    feat: create wallet list

--- context/ranbow/config/index.ts
+++ context/ranbow/config/index.ts
@@ -2,10 +2,20 @@
 
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import { mainnet, polygon, optimism, arbitrum, base } from 'wagmi/chains';
+import {
+    metaMaskWallet,
+    walletConnectWallet,
+  } from '@rainbow-me/rainbowkit/wallets';
 
 export const config = getDefaultConfig({
     appName: 'My RainbowKit App',
     projectId: 'b1e32aed6f915e982334fa11b7b6156b',
     chains: [mainnet, polygon, optimism, arbitrum, base],
+    wallets: [
+      {
+        groupName: 'Recommended',
+        wallets: [metaMaskWallet, walletConnectWallet]
+      }
+    ],
     ssr: true, // If your dApp uses server side rendering (SSR)
 });
