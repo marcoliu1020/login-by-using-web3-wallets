077d1d03fae78afc237164fa65e1a9e0dd023dd8
marco <a0931900802@gmail.com>
2025-04-21 11:58:31 +0800

    fix: no tron button

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
-import { TronConnectButton } from '@/components/TronConnectButton';
-import { TronProvider } from '@/context/tron';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen items-center p-2 pb-20 gap-5 font-[family-name:var(--font-geist-sans)]">
       <ConnectButton /> 
-      <TronProvider>
-        <TronConnectButton />
-      </TronProvider>
     </div>
   );
 }
