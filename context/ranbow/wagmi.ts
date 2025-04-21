a1ca97c5ae33f08cf52941a5e446f8cdcbaf69ad
marco <a0931900802@gmail.com>
2025-04-21 10:21:01 +0800

    feat: update chain list

--- context/ranbow/wagmi.ts
+++ context/ranbow/wagmi.ts
@@ -1,22 +1,22 @@
 "use client";
 
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { mainnet, polygon, optimism, arbitrum, base } from 'wagmi/chains';
 import {
-    metaMaskWallet,
-    walletConnectWallet,
-  } from '@rainbow-me/rainbowkit/wallets';
+  metaMaskWallet,
+  walletConnectWallet,
+} from '@rainbow-me/rainbowkit/wallets';
+import { arbitrum, base, bsc, mainnet } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
-    appName: 'My RainbowKit App',
-    projectId: 'b1e32aed6f915e982334fa11b7b6156b',
-    chains: [mainnet, polygon, optimism, arbitrum, base],
-    wallets: [
-      {
-        groupName: 'Recommended',
-        wallets: [metaMaskWallet, walletConnectWallet]
-      }
-    ],
-    ssr: true, // If your dApp uses server side rendering (SSR)
-    multiInjectedProviderDiscovery: false, // hide injected wallets
+  appName: 'My RainbowKit App',
+  projectId: 'b1e32aed6f915e982334fa11b7b6156b',
+  chains: [mainnet, base, arbitrum, bsc],
+  wallets: [
+    {
+      groupName: 'Recommended',
+      wallets: [metaMaskWallet, walletConnectWallet]
+    }
+  ],
+  ssr: true, // If your dApp uses server side rendering (SSR)
+  multiInjectedProviderDiscovery: false, // hide injected wallets
 });
