d736aaae1dd43aca05c8df031b1e2503788a5b0a
marco <a0931900802@gmail.com>
2025-04-21 11:52:12 +0800

    feat: update layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import { TronConnectButton } from '@/components/TronConnectButton';
+import { TronProvider } from '@/context/tron';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <ConnectButton />
+    <div className="flex flex-col min-h-screen items-center p-2 pb-20 gap-5 font-[family-name:var(--font-geist-sans)]">
+      <ConnectButton /> 
+      <TronProvider>
+        <TronConnectButton />
+      </TronProvider>
     </div>
   );
 }
